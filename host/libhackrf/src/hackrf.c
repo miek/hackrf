857392e22472875bc8cf6874ab2c22bccab18132
AlexandreRouma <alexandre.rouma@gmail.com>
2022-09-01 21:59:57 +0200

    libhackrf: add hackrf_open_by_fd function
    Add the hackrf_open_by_fd function that allows to open a hackrf device using a file descriptor.
    This is the only possible way to open a usb device on a non-rooted android device.

--- host/libhackrf/src/hackrf.c
+++ host/libhackrf/src/hackrf.c
@@ -833,6 +833,34 @@ int ADDCALL hackrf_open_by_serial(
 	return hackrf_open_setup(usb_device, device);
 }
 
+int ADDCALL hackrf_open_by_fd(int fd, hackrf_device** device)
+{
+#ifndef _WIN32
+	libusb_device_handle* usb_device;
+
+	if (fd < 0) {
+		return HACKRF_ERROR_INVALID_PARAM;
+	}
+
+	if (device == NULL) {
+		return HACKRF_ERROR_INVALID_PARAM;
+	}
+
+	int err = libusb_wrap_sys_device(g_libusb_context, (intptr_t) fd, &usb_device);
+	if (err) {
+		return HACKRF_ERROR_NOT_FOUND;
+	}
+
+	if (usb_device == NULL) {
+		return HACKRF_ERROR_NOT_FOUND;
+	}
+
+	return hackrf_open_setup(usb_device, device);
+#else
+	return HACKRF_ERROR_UNSUPPORTED;
+#endif
+}
+
 int ADDCALL hackrf_device_list_open(
 	hackrf_device_list_t* list,
 	int idx,
